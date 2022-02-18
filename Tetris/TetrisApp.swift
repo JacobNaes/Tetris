74580bb58cfab988b9b848e3e366b81743c02332
Jacob Naes <jacob_naes@icloud.com>
2022-02-18 13:37:00 -0500

    Initial Commit

---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by Jacob Naes on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
